8ded43f7da35f1d1b19805efada00cad6cc8b836
Corey <jcshott@gmail.com>
2015-10-22 08:33:21 -0700

    working on parsing command line arguments"

--- slack_dmInfo_retrieve.py
+++ slack_dmInfo_retrieve.py
@@ -1,18 +1,18 @@
-import requests, datetime, time, sys, os
+import requests, datetime, time, sys, os, argparse
 #make the timestamps go into a log file and then have the program read the last entry to get next block of messages to download. if nothing in log file, then have it default to getting all history
 
-def get_dm_info(outputfile):
-	"""appends to specified outputfile the messages in a DM channel.
-	takes in outputfile name and beginning timestamp. returns nothing, 
+def get_dm_info(output_file, start_timestamp=0):
+	"""appends to specified output_file the messages in a DM channel.
+	takes in output_file name and beginning timestamp. returns nothing, 
 	appends to specified file the following info: 
 	MM-DD-YY, time of message: user_name of sender - message text
 	"""
 
-	with open(outputfile, 'a') as f:
+	with open(output_file, 'a') as f:
 
 		query_params = {'token': mytoken,
 						'channel': "D076VAAKY",
-						# 'oldest': str(last_import_ts)
+						# 'oldest': str(start_timestamp)
 					       }
 		endpoint = 'https://slack.com/api/im.history'
 		response = requests.get(endpoint, params=query_params).json()
@@ -65,11 +65,23 @@ def get_user_name(user_id):
 		return None
 
 if __name__ == '__main__':
-	# try:
-	# 	last_import_ts = sys.argv[1]
-	# 	outputfile = sys.argv[2]	
-	# except IndexError:
-	# 	print "Try again. Please provide the last timestamp for which you want to pull message data as the 2nd command line argument and output filename for the third"
+
+	parser = argparse.ArgumentParser(description="Retrieve DM info from Slack")
+	parser.parse_args()
+	parser.add_argument('-o', '--o', action='store_const', dest='output_file', help='define output_file')
+	# if len(sys.argv) == 2:
+	# 	try:
+	# 		output_file = sys.argv[1]
+	# 		start_timestamp = sys.argv[2]	
+	# 	except:
+	# 		print "If you are providing both a starting timestamp and output file name, please provide the filename first and timestamp second"
 	mytoken = os.environ.get("SLACK_TOKEN")
 	get_dm_info('test.txt')
+
+	#if timestamp parameter given - use that (argparse library)
+	#if not - check if there's a log file and read last line (in a function above)
+	#if no log file or parameter given, get everything
+
+	#config file - put the channel, api key, output file name
+	#http://stackoverflow.com/questions/8225954/python-configuration-file-any-file-format-recommendation-ini-format-still-appr
 	
